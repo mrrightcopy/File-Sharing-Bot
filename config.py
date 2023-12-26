89ad665a6ff63c3a278225d4e6429a01e69cd14d
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2023-12-26 19:15:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "**🆔 @Quality_LinksZ 👈 Join Our Main Channel **\n"
+USER_REPLY_TEXT = "🆔 @Quality_LinksZ 👈 Join Our Main Channel"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
