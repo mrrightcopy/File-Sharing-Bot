50ce0c9da465a79d0e2e3adb8078b54a1ccac1b5
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2023-12-26 20:14:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-#TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5780282761:AAG9vcgAVK__K1btiy212Ao-9E_rlpelADw") #Serials Tv
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5780282761:AAG9vcgAVK__K1btiy212Ao-9E_rlpelADw") #Serials Tv
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6961541840:AAFY396GaaVLnm1cWd_aRkD5UV7vk7olPrA")
+#TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6961541840:AAFY396GaaVLnm1cWd_aRkD5UV7vk7olPrA") #Series Web
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "1923471"))
@@ -21,9 +21,9 @@ APP_ID = int(os.environ.get("APP_ID", "1923471"))
 API_HASH = os.environ.get("API_HASH", "fcdc178451cd234e63faefd38895c991")
 
 #Your db channel Id
-#CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002117688872")) #Tv Serials 
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002117688872")) #Tv Serials 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001773942005"))
+#CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001773942005")) #Series Web
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "807374433"))
